92af69da5b2be3afe789ddda9969b7fb2bf1cca1
Limbo20000 <78791711+Limbo20000@users.noreply.github.com>
2022-02-06 16:25:56 +0100

    Update RTKLibModule.py

--- Updated_Modules/RTKLibModule.py
+++ Updated_Modules/RTKLibModule.py
@@ -6,6 +6,7 @@ import wget
 import requests
 import patoolib
 import platform
+import json
 
 os_type=''
 
@@ -29,15 +30,24 @@ station=str(sys.argv[3])
 time=str(sys.argv[4])
 uid=str(sys.argv[5])
 
+doy="0"+doy
+
 year_for_filename= year[2:]
 
-temp=""
-f = open('config.txt', 'r')
-for line in f:
-        if(line.split(' ')[0]==sys.argv[5]):
-            temp=line.split(' ')[1]
-
-save_location=temp[6:]
+with open("config.json","r") as f:
+    json_data=json.loads(f.read())
+    f.close()
+    i=0
+    size=len(json_data['Users'])
+    while i!=size:
+        splited=str(json_data['Users'][i]).split(',')
+        user=splited[2]
+        user=user[10:-2]
+        if user==sys.argv[5]:
+            temp=splited[0]
+            temp=temp[10:-1]
+            save_location=temp
+        i=i+1
 
 zip_path= save_location +'/PildoBox' + station + year_for_filename + doy + time + ".raw.zip"
 print("zip:",zip_path)
@@ -116,13 +126,18 @@ if os.path.getsize(nav)<=4000:
     if os_type == 'Linux' or os_type == 'Darwin':
         nav = save_location + "/brdc" + doy + '0.21n'
 
-fp="rnx2rtkp -k rnxconfig.conf -p 0 -f 1 -f 2 -f 5 -t " + obs +" " + nav +" " + sbs +" -o " + save_location + station + year_for_filename + doy + time + ".pos"
+fp="rnx2rtkp -k rnxconfig.conf -p 0 -f 1 -f 2 -f 5 -t " + obs +" " + nav +" -o " + save_location + station + year_for_filename + doy + time + ".pos"
+##TODO meg egy feldolgozas sbs használatával
+fp2="rnx2rtkp -k rnxconfig2.conf " + obs +" " + nav +" " + sbs +" -o " + save_location + station + year_for_filename + doy + time +"_egnos"+ ".pos"
+
+#/usr/local/bin/rnx2rtkp -k ~/HC/util/SSSS_rtk.conf -r 4081881.803 1410011.610 4678199.731 -o $fname"_rtk.out" $fname$year2"o" $fname$year2"n" $fname$year2"l" $bute_ref_30
+
+##TODO ez lenne a kinematikus pozicionálás meghívása de valami rossz a config fájlal ha futtatom
+fp3="rnx2rtkp -k rnxconfig_kinematic.conf "+ obs +" " + nav +" " + sbs + "-r 4081881.803 1410011.610 4678199.731 -o "+save_location + station + year_for_filename + doy + time+"_kinematic"+".pos"
 #print("fp:",fp)
 
 os.system(fp)
-
-
-
-
-
+#os.system(fp2)
+##TODO mivel rossz kikommenteltem
+#os.system(fp3)
 

18e1721d7fd0180ed5a997a7c30a2254c9741a7b
Limbo20000 <78791711+Limbo20000@users.noreply.github.com>
2022-02-06 16:24:39 +0100

    Update DownloadModule.py

--- Updated_Modules/DownloadModule.py
+++ Updated_Modules/DownloadModule.py
@@ -1,7 +1,9 @@
+import json
 import wget
 import sys
 import os
 import requests
+
 
 #http://152.66.5.8/~tbence/hc/data/Y2021/D010/PildoBox205/PildoBox20521010a.raw.zip
 
@@ -10,43 +12,11 @@ import requests
 url = 'http://152.66.5.8/~tbence/hc/data/'
 ##TODO test under linux and windows
 
-#Basic test function for windows
-def test_mode(input:str):
-    if sys.argv[1]=='Test':
-        url="http://152.66.5.8/~tbence/hc/data/Y2021/D010/PildoBox205/PildoBox20521010b.raw.zip"
-        f = open('config.txt', 'r')
-        for line in f:
-            if (line.split(' ')[0] == sys.argv[2]):
-                temp = line.split(' ')[1]
-
-        save_location=temp[6:]
-
-        if not os.path.exists(save_location):
-            os.mkdir(save_location)
-            print("Directory created!")
-
-
-        response = requests.get(url)
-
-        if response.status_code !=200:
-            print("URL Error!")
-            exit()
-
-        if response.status_code==200 and not os.path.exists(save_location+'\\'+'PildoBox20521010b.raw.zip'):
-            download = wget.download(url, save_location)
-            exit()
-
-        if os.path.exists(save_location+'\\'+'PildoBox20521010b.raw.zip'):
-            print("Existing file!")
-            exit()
-
-test_mode(sys.argv[1])
-
 #check number of args
 if len(sys.argv) != 6:
     print("arguments: yyy ddd sss h")
-    print("where: yyy  y=year, ddd=day of year, sss=station id, h=hourly session with abc")
-    print("an example 2021 10 205 a")
+    print("where: yyy  y=year, ddd=day of year, sss=station id, h=hourly session with abc, uid=user id")
+    print("an example 2021 10 205 a John")
     exit()
 
 #format args
@@ -61,13 +31,29 @@ uid=str(sys.argv[5])
 #Read config file and get save_location by user id
 save_location=""
 
-f = open('config.txt', 'r')
-for line in f:
-        if(line.split(' ')[0]==sys.argv[5]):
-            temp=line.split(' ')[1]
+#f = open('config.txt', 'r')
+#for line in f:
+#       if(line.split(' ')[0]==sys.argv[5]):
+#            temp=line.split(' ')[1]
 
 
-save_location=temp[6:]
+with open("config.json","r") as f:
+    json_data=json.loads(f.read())
+    f.close()
+    i=0
+    size=len(json_data['Users'])
+    while i!=size:
+        splited=str(json_data['Users'][i]).split(',')
+        user=splited[2]
+        user=user[10:-2]
+        if user==sys.argv[5]:
+            temp=splited[0]
+            temp=temp[10:-1]
+            save_location=temp
+        i=i+1
+
+print(save_location)
+
 
 #directory to save data locally. If directory does not exist create it
 if not os.path.exists(save_location):
@@ -76,14 +62,18 @@ if not os.path.exists(save_location):
 
 year_for_filename= year[2:]
 
-
 #full url to download
 full_url = url + '/Y' + year + '/D' + doy + '/PildoBox' + station + '/PildoBox' + station + year2 + doy + session + '.raw.zip'
 
+#BUTE data download for chosen day
+#http://152.66.5.8/~tbence/hc/data/Y2021/D010/bute/BUTE010a.21d.zip
+kine_url = url + 'Y' + year + '/D' + doy + '/bute'+'/BUTE' + doy + session + '.'+year2+'d.zip'
+print("url:"+kine_url)
+
+
 station='PildoBox'+station
-dupe_check= save_location +'\\' + station + year_for_filename + doy + session + '.raw.zip'
-
-
+dupe_check= save_location +'/' + station + year_for_filename + doy + session + '.raw.zip'
+dupe2=save_location +'/BUTE' + doy + session+'.' + year2+'d.zip'
 
 response=requests.get(full_url)
 if response.status_code!=200:
@@ -92,9 +82,7 @@ if response.status_code!=200:
 if response.status_code==200 and not os.path.exists(dupe_check) :
     fn = wget.download(full_url, out=save_location)
 
-
-
-
-
+if response.status_code==200 and not os.path.exists(dupe2):
+    kinematic = wget.download(kine_url, save_location)
 
 

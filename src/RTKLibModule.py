168d57d6e35ace87c9094e5b470c51675c53efcf
Bence Takacs <tbence@shadow.geod.bme.hu>
2022-11-10 07:50:35 +0100

    item 'x' added to sessions

--- src/RTKLibModule.py
+++ src/RTKLibModule.py
@@ -24,7 +24,7 @@ def hour2session(hour):
     """
 
     session_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w']
+                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x']
     return session_list[int(hour)]
 
 def date2doy(dt):
